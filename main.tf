d33f47dd1278dd3f1828bd1835b899924fabc276
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2024-02-05 21:07:06 +0000

    Update main.tf
    provifdr

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 provider "google" {
   region  = "us-east1"
-  //project = "solar-theory-245311"
-  //default_labels = {
-    //GOOGLE_CREDENTIALS = "O9xgrXKqDAKlW8jeMUw9IO2XJ7S11NlQ//UlyPbt"
-  //}
 }
 resource "google_compute_instance" "standalone-instance-1" {
   name         = "standalone-instance-1"
