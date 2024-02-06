368305236c8d9dadd96d1f642dd323b785cd3540
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2024-02-06 17:46:01 +0000

    Update main.tf
    Added space between resources

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ provider "google" {
     //,"https://googleapis.com/auth/userinfo.email"
   ]
 }
+
 resource "google_compute_instance" "standalone-instance-1" {
   name         = "standalone-instance-1"
   machine_type = "n1-standard-1"
